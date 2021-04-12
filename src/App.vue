2a20eb4627a00d5aed99d489ce252b8c1f3d2606
rc <rc@hudl.us>
2021-04-11 18:36:00 -0700

    more prep and workflow testing

--- src/App.vue
+++ src/App.vue
@@ -12,11 +12,11 @@
       <v-spacer></v-spacer>
 
       <v-btn
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
+        href="https://github.com/lfo-calculator/vue-hi"
         target="_blank"
         text
       >
-        <span class="mr-2">Latest Release</span>
+        <span class="mr-2">Github repo</span>
         <v-icon>mdi-open-in-new</v-icon>
       </v-btn>
     </v-app-bar>
